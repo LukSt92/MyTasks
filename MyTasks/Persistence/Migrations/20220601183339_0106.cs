b3efbc6cc936e5b9e727602268252ab4510f1c39
LukSt92 <stach.stachu@gmail.com>
2022-06-01 21:00:06 +0200

    added database logic

---
+++ MyTasks/Persistence/Migrations/20220601183339_0106.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyTasks.Data.Migrations
+{
+    public partial class _0106 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Term",
+                table: "Tasks",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Term",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Time",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
--- MyTasks/Persistence/Repositories/TaskRepository.cs
+++ MyTasks/Persistence/Repositories/TaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyTasks.Core.Models.Domains;
+﻿using Microsoft.EntityFrameworkCore;
+using MyTasks.Core.Models.Domains;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,40 +8,75 @@ namespace MyTasks.Persistence.Repositories
 {
     public class TaskRepository
     {
+        private ApplicationDbContext _context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<Task> Get(string userId,
             bool isExecuted = false, int categoryId = 0, string title = null)
         {
-            throw new NotImplementedException();
+            var tasks = _context.Tasks
+                .Include(x => x.Category)
+                .Where(x => x.UserId == userId &&
+                x.IsExecuted == isExecuted);
+
+            if (categoryId != 0)
+                tasks = tasks.Where(x => x.CategoryId == categoryId);
+
+            if (!string.IsNullOrWhiteSpace(title))
+                tasks = tasks.Where(x => x.Title.Contains(title));
+
+            return tasks.OrderBy(x => x.Term).ToList();
         }
 
         public IEnumerable<Category> GetCategories()
         {
-            throw new NotImplementedException();
+            return _context.Categories.OrderBy(x => x.Name).ToList();
         }
 
-        internal Task Get(int id, string userId)
+        public Task Get(int id, string userId)
         {
-            throw new NotImplementedException();
+            var task = _context.Tasks.Single(x => x.Id == id && x.UserId == userId);
+
+            return task;
         }
 
-        internal void Add(Task task)
+        public void Add(Task task)
         {
-            throw new NotImplementedException();
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
         }
 
-        internal void Update(Task task)
+        public void Update(Task task)
         {
-            throw new NotImplementedException();
+            var taskToUpdate = _context.Tasks.Single(x => x.Id == task.Id);
+
+            taskToUpdate.CategoryId = task.CategoryId;
+            taskToUpdate.Description = task.Description;
+            taskToUpdate.IsExecuted = task.IsExecuted;
+            taskToUpdate.Term = task.Term;
+            taskToUpdate.Title = task.Title;
+
+            _context.SaveChanges();
         }
 
-        internal void Delete(int id, string userId)
+        public void Delete(int id, string userId)
         {
-            throw new NotImplementedException();
+            var taskToDelete = _context.Tasks.Single(x => x.Id == id && x.UserId == userId);
+
+            _context.Tasks.Remove(taskToDelete);
+            _context.SaveChanges();
         }
 
-        internal void Finish(int id, string userId)
+        public void Finish(int id, string userId)
         {
-            throw new NotImplementedException();
+            var taskToUpdate = _context.Tasks.Single(x => x.Id == id && x.UserId == userId);
+
+            taskToUpdate.IsExecuted = true; ;
+            _context.SaveChanges();
         }
     }
 }
